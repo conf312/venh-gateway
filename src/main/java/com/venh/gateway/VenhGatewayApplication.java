3c4961d921e7b2efbb3e8b27ee813540fed17dc3
venganza312 <gpfm312@gmail.com>
2021-12-07 10:38:17 +0900

    initialize gateway service

---
+++ src/main/java/com/venh/gateway/VenhGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.venh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenhGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenhGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venh/gateway/VenhGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venh.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenhGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
